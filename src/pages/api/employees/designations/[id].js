207251e32dd934c26867f72e92fab16759451b52
Tanvi Kadam <91339637+tzevk@users.noreply.github.com>
2025-07-19 23:16:46 +0530

    just get deployed

--- src/pages/api/employees/designations/[id].js
+++ src/pages/api/employees/designations/[id].js
@@ -1,4 +1,4 @@
-import { executeQuery } from '../../../../../lib/db';
+import { executeQuery } from '../../../../lib/db';
 
 export default async function handler(req, res) {
   const { id } = req.query;
